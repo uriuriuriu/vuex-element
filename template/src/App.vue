e56064cc6679e51ac797bb94fd1fd96d01abd3bb
uriuriuriu <uriuriuriu@gmail.com>
2017-10-17 22:49:15 +0900

    init

---
+++ template/src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    router-view
+    el-time-select(v-model="value1", :picker-options="{start: '08:30', step: '00:15', end: '18:30'}", placeholder="Select time")
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      value1: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
